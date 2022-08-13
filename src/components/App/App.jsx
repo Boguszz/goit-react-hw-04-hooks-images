db26e4e6cf0a9a793c39780b5094cf3a6338cfc3
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-08-13 11:06:49 +0200

    cleaned up the app component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,74 +1,29 @@
-import { useState, useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import fetchImages from '../../services/images-api';
 import Searchbar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery';
 import Button from 'components/Button';
 import Loader from 'components/Loader';
 import Modal from 'components/Modal';
 
+import useGetImages from 'hooks/useGetImages';
+import useModal from 'hooks/useModal';
+
 function App() {
-  const [query, setQuery] = useState('');
-  const [page, setPage] = useState(1);
-  const [totalImages, setTotalImages] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [images, setImages] = useState(null);
-  const [error, setError] = useState(null);
-  const [currentImageUrl, setCurrentImageUrl] = useState(null);
-  const [currentImageDescription, setCurrentImageDescription] = useState(null);
+  const {
+    getSearchRequest,
+    onNextFetch,
+    totalImages,
+    isLoading,
+    images,
+    error,
+  } = useGetImages();
 
-  if (error) {
-    console.log(error);
-  }
-
-  useEffect(() => {
-    if (query !== '') {
-      setIsLoading(prevIsLoading => !prevIsLoading);
-      fetchImages(query, page)
-        .then(({ hits, totalHits }) => {
-          const imagesArray = hits.map(hit => ({
-            id: hit.id,
-            description: hit.tags,
-            smallImage: hit.webformatURL,
-            largeImage: hit.largeImageURL,
-          }));
-          setTotalImages(totalHits);
-          return imagesArray;
-        })
-        .then(imagesArray => {
-          if (page === 1) {
-            setImages(imagesArray);
-          }
-          return imagesArray;
-        })
-        .then(imagesArray => {
-          if (page !== 1) {
-            setImages(prevImages => [...prevImages, ...imagesArray]);
-          }
-        })
-        .catch(error => setError(error))
-        .finally(() => setIsLoading(prevIsLoading => !prevIsLoading));
-    }
-  }, [page, query]);
-
-  const getSearchRequest = query => {
-    setQuery(query);
-    setPage(1);
-  };
-
-  const onNextFetch = () => setPage(prevPage => prevPage + 1);
-
-  const toggleModal = () => setShowModal(prevShowModal => !prevShowModal);
-
-  const openModal = e => {
-    if (e.target.nodeName === 'IMG') {
-      setShowModal(prevShowModal => !prevShowModal);
-      setCurrentImageUrl(e.target.dataset.large);
-      setCurrentImageDescription(e.target.alt);
-    }
-  };
+  const {
+    openModal,
+    toggleModal,
+    showModal,
+    currentImageUrl,
+    currentImageDescription,
+  } = useModal();
 
   return (
     <>
@@ -90,7 +45,7 @@ function App() {
         />
       )}
 
-      <ToastContainer />
+      {error && <p>{error}</p>}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import App from 'components/App';
 import 'style/index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename="gallery">
       <App />
+      <ToastContainer />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
